b036fc077f0e2740a042d3bb3ab9aa4d669d5cd6
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 21:24:04 +1000

    Create arithmeticOperations.py

---
+++ arithmeticOperations.py
@@ -0,0 +1,6 @@
+first_number_user = int(input())
+second_number_user = int(input())
+
+print(first_number_user, '+', second_number_user, '=', first_number_user + second_number_user)
+print(first_number_user, '-', second_number_user, '=', first_number_user - second_number_user)
+print(first_number_user, '*', second_number_user, '=', first_number_user * second_number_user)
