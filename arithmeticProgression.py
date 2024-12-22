8530827b62dddcfb61bd1d00e8bd3e47846b051a
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 21:29:08 +1000

    Create arithmeticProgression.py

---
+++ arithmeticProgression.py
@@ -0,0 +1,7 @@
+number_user_a1 = int(input())
+number_user_d = int(input())
+number_user_n = int(input())
+
+arithmeticProgression = number_user_a1 + (number_user_d * (number_user_n - 1))
+
+print(arithmeticProgression)
