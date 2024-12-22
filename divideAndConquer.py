b2fd0c4c20041e3a473b45bc3a440e63875433df
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 21:36:50 +1000

    Create divideAndConquer.py

---
+++ divideAndConquer.py
@@ -0,0 +1,8 @@
+number_user_x = int(input())
+
+number_user_x2 = number_user_x * 2
+number_user_x3 = number_user_x * 3
+number_user_x4 = number_user_x * 4
+number_user_x5 = number_user_x * 5
+
+print(number_user_x, number_user_x2, number_user_x3, number_user_x4, number_user_x5, sep='---')
