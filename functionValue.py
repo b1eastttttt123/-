8dee331b62d5d84091189575fbd2da07a4ad8d51
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 21:01:44 +1000

    Create functionValue.py

---
+++ functionValue.py
@@ -0,0 +1,6 @@
+first_number_user = int(input())
+second_number_user = int(input())
+
+result = (3 * ((first_number_user + second_number_user) ** 3)) + (275 * (second_number_user ** 2)) - (127 * first_number_user) - 41
+
+print(result)
