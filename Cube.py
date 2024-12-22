bf3c5af10cf43c73cc1c885678d3f58a7602dd53
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 20:51:47 +1000

    Create Cube.py

---
+++ Cube.py
@@ -0,0 +1,11 @@
+# Enter edge length
+number_user = int(input('Enter edge length: '))  # Rib length
+
+# Formulas
+V = number_user ** 3  # Cube volume
+S = 6 * (number_user ** 2)  # Total surface area
+
+# Results
+print('Cube volume:', V)  # Cube volume
+print('Total surface area:', S)  # Total surface area
+
