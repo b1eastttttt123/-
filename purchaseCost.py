879a817771187bab7de70d24ca11806544eb25c6
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 21:18:53 +1000

    Create purchaseCost.py

---
+++ purchaseCost.py
@@ -0,0 +1,8 @@
+cost_monitor = int(input())
+cost_system_unit = int(input())
+cost_keyboard = int(input())
+cost_mouse = int(input())
+
+three_computers = (cost_mouse + cost_monitor + cost_keyboard + cost_system_unit) * 3
+
+print(three_computers)
