bc2977ba5c72e1fbe6c5518183eb6a02720ccf46
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-22 20:33:44 +1000

    Create SumOfThreeNumbers.py

---
+++ SumOfThreeNumbers.py
@@ -0,0 +1,7 @@
+first_number_user = int(input())
+second_number_user = int(input())
+third_number_user = int(input())
+
+summ = first_number_user + second_number_user + third_number_user
+
+print(summ)
