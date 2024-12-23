c22d7585472382bedeabb34f5420ad2286d78cf6
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-23 17:39:47 +1000

    Create geometricProgression.py

---
+++ geometricProgression.py
@@ -0,0 +1,7 @@
+first_number_user_b1 = int(input())
+second_number_user_q = int(input())
+third_number_user_n1 = int(input())
+
+result = first_number_user_b1 * (second_number_user_q ** (third_number_user_n1 - 1))
+
+print(result)
